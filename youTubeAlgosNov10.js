00e1074bca14e7c9dc8d95c8d264fac4dd2f4566
Bohdan Mykykytey <bohdan.mykytey@gamil.com>
2020-11-11 13:07:41 -0500

    practice practice practice;

--- youTubeAlgosNov10.js
+++ youTubeAlgosNov10.js
@@ -20,4 +20,29 @@ function longestWord(sen) {
   return longestWordArray.length === 1 ? longestWordArray[0] : longestWordArray;
 }
 
-console.log(longestWord("Hello there my name is Brad"));
+// console.log(longestWord("Hello there my name is Brad"));
+
+//CHALLANGE 2: ARRAY CHUNKING
+//split an array into chunked arrays of a specific length
+//ex: chunkArray([1,2,3,4,5,6,7], 3) === [[1,2,3],[4,5,6],[7]];
+//ex: chunkArray([1,2,3,4,5,6,7], 2) === [[1,2],[3,4],[5,6],[7]];
+
+
+//solution 1
+function chunkArray(arr, len) {
+  //initialize a chunked arr
+  const chunkArr = [];
+  //set index to 0;
+  let i = 0;
+  //loop while index is less than array.length
+  while (i < arr.length) {
+    //slice out from the index to the index + chunk length and push into the chunked array
+    chunkArr.push(arr.slice(i, i + len));
+    //incriment by chunk length
+    i += len;
+  }
+  return chunkArr;
+}
+
+console.log(chunkArray([1,2,3,4,5,6,7], 2))
+
