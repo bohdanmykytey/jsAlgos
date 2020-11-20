5db9e0f8623f3c5c453e3b248b210c34cf6deca2
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2020-11-20 14:42:27 -0500

    more jsCardio

---
+++ leetCode.js
@@ -0,0 +1,10 @@
+//Reverse int
+
+function reverseInt(...input) {
+    let result = input.toString().split('').reverse().join('');
+    let int = parseInt(result)
+    return int;
+}
+
+console.log(reverseInt(123456))
+
