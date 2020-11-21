583f4baaf8f8a3e2bc000050756a3d7d1114bc20
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2020-11-21 16:04:21 -0500

    refactored first problem

--- codeWarsChallanges.js
+++ codeWarsChallanges.js
@@ -3,13 +3,8 @@
 //2 memebership tiers: open and senior. 
 //if at or over 55 age and handicap greater than 7 === open else regular
 
-function categorizeMemberships(memberInfo)    {
-    return memberInfo.map((info) => {
-        if(info[0] >= 55 && info[1] > 7) {
-            return 'Senior'
-        }   
-        return "Open"
-    })
+function categorizeMemberships(memberInfo) {
+    return memberInfo.map(([age, handicap]) => (age => 55 && handicap > 7) ? "Senior" : "Open");
 }
 
 console.log(categorizeMemberships([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
